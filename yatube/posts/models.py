67c152c298c8bca0b90c37f67b72392d510aaaec
Alex Efremov <alexefremov74@gmail.com>
2022-12-06 01:13:31 +0500

    ревью

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -19,12 +19,14 @@ class Post(models.Model):
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='posts'
     )
     group = models.ForeignKey(
         Group,
         blank=True,
         null=True,
-        on_delete=models.CASCADE,
-        related_name='posts',
+        on_delete=models.SET_NULL,
     )
+
+    class Meta:
+        default_related_name = "posts"
+        ordering = ['-pub_date']
